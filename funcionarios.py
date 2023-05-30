8a3e07e0949b2a85456efe385ddbb97fa7194931
Vinicius Gabriel <vinicius-gabriel-guilherme@hotmail.com>
2023-05-30 13:41:28 -0300

    create crud funcionarios

--- funcionarios.py
+++ funcionarios.py
@@ -19,4 +19,24 @@ def readfunc():
     except error as ex:
         print(ex)
 
+def updateFunc(novo_nome,id):
+    try:
+        novo_nome = input("NOME:")
+        curso.execute(f"UPDATE funcionarios SET Nome='{novo_nome}' WHERE id={id}")
+        banco.commit()
+    except error as ex:
+        print(ex)
+    finally:
+        print("Dados Atualizados com Sucesso!")
+
+
+def deleteFunc(id):
+    try:
+        id = input("ID:")
+        curso.execute(f"DELETE FROM funcionarios WHERE ID={id}")
+        banco.commit()
+    except error as ex:
+        print(ex)
+    finally:
+        print("Funcionario Deletado Com sucesso!")
 
